229bf135b030354d29f9bc84748b98416323182c
GiordanoSasso <120652341+GiordanoSasso@users.noreply.github.com>
2024-05-11 19:36:58 -0300

    estudos 11/05

---
+++ OoComposicao/Desafio/Item.java
@@ -0,0 +1,12 @@
+package OoComposicao.Desafio;
+
+public class Item {
+    
+    final Produto produto;
+    final int quantidade;
+
+    Item(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+}
---
+++ OoComposicao/Desafio/Produto.java
@@ -0,0 +1,14 @@
+package OoComposicao.Desafio;
+
+public class Produto {
+    
+    String nome;
+    double preco;
+
+    Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+
+}
